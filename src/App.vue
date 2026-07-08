2973fa521466f0e452a396d01d6200314a06174f
delbaze <56109313+delbaze@users.noreply.github.com>
2026-07-08 11:40:53 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import ProductCard from '@/components/ProductCard.vue';
+import type { Product } from '@/types/Product';
+import { ref } from 'vue';
+
+
+
+const testProduct: Product = {
+  id: 1,
+  title: 'Produit de test',
+  price: 29,
+  description: 'Juste pour vérifier que ProductCard fonctionne',
+  images: ['https://placehold.co/300'],
+  category: { id: 1, name: 'Test' }
+}
+let toto = ref("test");
+
+
+function coucou(productId: number) {
+  console.log(`coucou depuis le produit id : ${productId}`);
+  console.log("toto value", toto.value);
+  toto.value = "coucou";
+}
+
+function deleteProduct(productId: number) {
+  console.log(`je voudrais supprimé le produit id : ${productId}`)
+}
+
+</script>
+
+<template>
+  <ProductCard :product="testProduct" @say-coucou="coucou" @delete-prod="deleteProduct" />
+  <p v-once>{{ toto.toUpperCase() }}</p>
+  <p>{{ toto.toUpperCase() }}</p>
+</template>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { Product } from '@/types/Product';
+import { ref } from 'vue';
+// const product: Product = { title: "toto" }
+defineProps<{ product: Product }>()
+const emit = defineEmits<{ 'say-coucou': [productId: number], 'delete-prod': [productId: number] }>()
+const isLoading = ref(false);
+// const toto = "test";
+
+// const state = ref({
+//     products: [] as Product[],
+//     isLoading: true,
+//     searchQuery: ''
+// })
+
+const demo = ref<Product[]>([{
+    id: 1,
+    title: 'Produit de test 0',
+    price: 29,
+    description: 'Juste pour vérifier que ProductCard fonctionne',
+    images: ['https://placehold.co/300'],
+    category: { id: 1, name: 'Test' }
+},
+{
+    id: 2,
+    title: 'Produit de test 1',
+    price: 29,
+    description: 'Juste pour vérifier que ProductCard fonctionne',
+    images: ['https://placehold.co/300'],
+    category: { id: 1, name: 'Test' }
+}])
+</script>
+
+<template>
+    <h1>{{ product.title }}</h1>
+    <h2 v-if="isLoading">Chargement en cours</h2>
+    <!-- <h2 v-else-if="isLoading">Chargement en cours</h2> -->
+
+    <ul>
+        <!-- <li v-for="(p, index) in demo" :key="index">{{ p.title }}</li> -->
+        <li v-for="p in demo" :key="p.title">{{ p.title }}</li>
+    </ul>
+    <button @click="emit('say-coucou', product.id)">Dire coucou</button>
+    <button @click="emit('delete-prod', product.id)">Supprimer le produit</button>
+</template>
